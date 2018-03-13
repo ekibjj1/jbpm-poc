f34975e2a891b920d45806ae7b37582aea16fe84
ahernper <ahernper@BIO-B5WFJ72.usersad.everis.int>
2018-03-13 14:21:47 +0100

    Corrigiendo clases erronear

--- jbpm-poc/src/main/java/com/orange/jbpm_poc/BundledProductOffering.java
+++ jbpm-poc/src/main/java/com/orange/jbpm_poc/BundledProductOffering.java
@@ -8,107 +8,78 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-"id",
-"href",
-"name"
-})
+@JsonPropertyOrder({ "id", "href", "name" })
 public class BundledProductOffering {
 
-@JsonProperty("id")
-private String id;
-@JsonProperty("href")
-private String href;
-@JsonProperty("name")
-private String name;
-@JsonIgnore
-private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+	@JsonProperty("id")
+	private String id;
+	@JsonProperty("href")
+	private String href;
+	@JsonProperty("name")
+	private String name;
+	@JsonIgnore
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-/**
-* No args constructor for use in serialization
-* 
-*/
-public BundledProductOffering() {
-}
+	/**
+	 * No args constructor for use in serialization
+	 * 
+	 */
+	public BundledProductOffering() {
+	}
 
-/**
-* 
-* @param id
-* @param name
-* @param href
-*/
-public BundledProductOffering(String id, String href, String name) {
-super();
-this.id = id;
-this.href = href;
-this.name = name;
-}
+	/**
+	 * 
+	 * @param id
+	 * @param name
+	 * @param href
+	 */
+	public BundledProductOffering(String id, String href, String name) {
+		super();
+		this.id = id;
+		this.href = href;
+		this.name = name;
+	}
 
-@JsonProperty("id")
-public String getId() {
-return id;
-}
+	@JsonProperty("id")
+	public String getId() {
+		return id;
+	}
 
-@JsonProperty("id")
-public void setId(String id) {
-this.id = id;
-}
+	@JsonProperty("id")
+	public void setId(String id) {
+		this.id = id;
+	}
 
-@JsonProperty("href")
-public String getHref() {
-return href;
-}
+	@JsonProperty("href")
+	public String getHref() {
+		return href;
+	}
 
-@JsonProperty("href")
-public void setHref(String href) {
-this.href = href;
-}
+	@JsonProperty("href")
+	public void setHref(String href) {
+		this.href = href;
+	}
 
-@JsonProperty("name")
-public String getName() {
-return name;
-}
+	@JsonProperty("name")
+	public String getName() {
+		return name;
+	}
 
-@JsonProperty("name")
-public void setName(String name) {
-this.name = name;
-}
+	@JsonProperty("name")
+	public void setName(String name) {
+		this.name = name;
+	}
 
-@JsonAnyGetter
-public Map<String, Object> getAdditionalProperties() {
-return this.additionalProperties;
-}
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
 
-@JsonAnySetter
-public void setAdditionalProperty(String name, Object value) {
-this.additionalProperties.put(name, value);
-}
-
-@Override
-public String toString() {
-return new ToStringBuilder(this).append("id", id).append("href", href).append("name", name).append("additionalProperties", additionalProperties).toString();
-}
-
-@Override
-public int hashCode() {
-return new HashCodeBuilder().append(id).append(additionalProperties).append(name).append(href).toHashCode();
-}
-
-@Override
-public boolean equals(Object other) {
-if (other == this) {
-return true;
-}
-if ((other instanceof BundledProductOffering) == false) {
-return false;
-}
-BundledProductOffering rhs = ((BundledProductOffering) other);
-return new EqualsBuilder().append(id, rhs.id).append(additionalProperties, rhs.additionalProperties).append(name, rhs.name).append(href, rhs.href).isEquals();
-}
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
 
 }
